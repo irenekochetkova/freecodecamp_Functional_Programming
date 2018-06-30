eae73f2d9daf5030a366a01b391d4fd6eac27d58
Irina <irenekochetkova@gmail.com>
2018-06-30 13:04:40 -0700

    added the solution of the task Use the map Method to Extract Data from an Array

---
+++ 06_Refactor_Global_Variables_Out_of_Functions.js
@@ -0,0 +1,37 @@
+// Refactor (rewrite) the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of an array. The remove function should remove the given bookName from an array. Both functions should return an array, and any new parameters should be added before the bookName one.
+// *****************************************************************
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (bookList, bookName) {
+  let copyBook = [...bookList];
+    copyBook.push(bookName);
+   
+  return copyBook;
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (bookList, bookName) {
+  let copyBook = [...bookList];
+  let index = copyBook.indexOf(bookName);
+    if (index >= 0) {
+      copyBook.splice(index, 1)
+      return copyBook;
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
